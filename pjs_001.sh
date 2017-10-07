5213ff1e41dbdf690f01e7216e7fbb8263ccd992
Unknown <michal.jazowski@student.uj.edu.pl>
2017-10-07 10:59:21 +0200

    001 - changed bash script to correctly correspond with requirements

--- pjs_001.sh
+++ pjs_001.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#jazowski_michal
+#jazowski_michal grupa 2
 
 usage() {
-  echo "Wyswietla informacje o obecnym uzytkowniku"
+  echo "Wyswietla informacje o obecnym uzytkowniku - login: Imie i Nazwisko"
   echo "Uzycie"
   echo "$0"
   echo "-h | --help Wyswietl pomoc"
   echo "-q | --quiet Tryb cichy"
+}
+rubbish_usage() {
+  echo "Niezrozumiale polecenie"
+  usage
 }
 
 validate_usage() {
@@ -14,51 +18,38 @@ validate_usage() {
   local is_quiet=false
   local is_rubbish=false
   while test $# -gt 0; do
-    case "$1" in
-      -h|--help)
-        is_help=true
-      ;;
-      -q|--quiet)
-        is_quiet=true
-      ;;
-      *)
-        is_rubbish=true
-      ;;
-    esac
+    if [ $1 == -h ] || [ $1 == --help ]; then
+      is_help=true
+    elif [ $1 == -q ] || [ $1 == --quiet ]; then
+      is_quiet=true
+    elif [[ $1 == -* ]] || [[ $1 == --* ]]; then
+      is_rubbish=true
+    fi
     shift
   done
-  #echo "$is_help $is_quiet $is_rubbish"
-
-  if [ "$is_help" = true ]; then
-    if [ "$is_rubbish" = false ]; then
-      #echo "help with 0"
-      usage
-      exit 0
-    else
-      #echo "help with 1"
-      usage
-      exit 1
-    fi
-  fi
-  if [ "$is_quiet" = true ]; then
+  #echo "$is_help $is_quiet $is_rubbish
+  if [ "$is_rubbish" == true ]; then
+    #echo "rubbish with 1"
+    rubbish_usage
+    exit 1
+  elif [ "$is_help" == true ]; then
+    #echo "help with 0"
+    usage
+    exit 0
+  elif [ "$is_quiet" == true ]; then
     #echo "quiet with 0"
     exit 0
   fi
-  if [ "$is_rubbish" = true ]; then
-    #echo "rubbish with 1"
-    usage
-    exit 1
-  fi
-
 }
 get_and_show_user_info() {
   # pierwsza znaleziona metoda, ale nie zawsze jest zainstalowana
   # finger -l "$USER" | grep -e Name:
-  user_name=$(getent passwd $USER | cut -d : -f 5 a | cut -d , -f 1)
+  user_name=$(getent passwd $USER | cut -d : -f 5 | cut -d , -f 1)
   if [ -z "$user_name" ]; then
-    user_name=$(echo $USER)
+    echo $USER
+    exit 0
   fi
-  echo "uzytkownik: $user_name"
+  echo "$USER: $user_name"
 }
 #=========
 validate_usage $@

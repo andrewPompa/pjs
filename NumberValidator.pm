d064fe9dced58e4d97bece02b54e7e57cf9da660
mijo <mijo@ailleron.com>
2018-01-14 20:40:11 +0100

    [PERL] Tenth script, NumberValidator module extended

--- NumberValidator.pm
+++ NumberValidator.pm
@@ -1,8 +1,8 @@
+#!/usr/bin/perl
 package NumberValidator;
 use strict;
 use warnings FATAL => 'all';
 
-#!/usr/bin/perl
 
 sub isInt {
     return $_[0] =~ /^\d+$/;
@@ -10,6 +10,28 @@ sub isInt {
 
 sub isScientificNotation {
     return $_[0] =~ /^(:?[-+]?\.\d+|[-+]?\d+\.?\d*)(:?[eEdDQ\^][-+]?\d+)?$/;
+}
+
+sub isValidMarkRow {
+    return $_[0] =~ /^(\w+\s){2}(:?[-+]?\d\.\d+|[-+]?\d|\d\.\d+[-+]?|\d[-+]?)$/;
+}
+sub convertMarkToNumber {
+    my $valueToSet = 0;
+    my $number = $_[0];
+
+    $number = $number;
+    if ($_[0] =~ /-/) {
+        $valueToSet = -0.25;
+    }
+    if ($_[0] =~ /\+/) {
+        $valueToSet = 0.25;
+    }
+    if ($_[0] =~ /\d+-/ || $_[0] =~ /\d+\+/) {
+        $number = substr $number, 0, - 1;
+    }
+    $number = abs($number);
+    $number += $valueToSet;
+    return $number;
 }
 
 1;

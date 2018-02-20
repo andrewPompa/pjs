a470011b7334271785950e1d1f1a4451831873c9
mijo <michal.jazowski@student.uj.edu.pl>
2018-02-21 00:02:41 +0100

    [BASH] Effectively final Usage and input validation to scripts added

--- pass-out-scripts/build.sh
+++ pass-out-scripts/build.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+usage() {
+  echo "Przykładowy skrypt uruchamiany przez git hook'a. Jest to namiastka narzędzia do Continuous Integration i Continuous Deployment"
+  echo "Pobiera z repozytorium i buduje, najpierw, frontend aplikacji (NodeJS)."
+  echo "Następnie pliki wynikowe przenoszone są do odpowiedniego katalogu dla pobranego z repo backendu (Spring Boot i maven)"
+  echo "Przed uruchomieniem aplikacji zabijany jest proces z wcześniejszą wersją"
+  echo "W tak przygotowanym środowisku uruchamiana jest aplikacja po świeżym push'u"
+  echo "Użycie:"
+  echo "$0 sciezka-do-repo-frontendu sciezka-do-repo-backendu nazwa-brancha"
+  echo "sciezka-do-repo-frontendu ścieżka do repozytorium z którego zostanie pobrany frontend aplikacji"
+  echo "sciezka-do-repo-backend ścieżka do repozytorium z którego zostanie pobrany backend aplikacji"
+  echo "nazwa-brancha branch z którego zostanie pobrany kod, taki sam dla obu aplikacji"
+}
+validate_usage() {
+  local is_help=false
+  while test $# -gt 0; do
+    if [ $1 == -h ] || [ $1 == --help ]; then
+      is_help=true
+    fi
+    shift
+  done
+  if [ "$is_help" == true ]; then
+    usage
+    exit 0
+  fi
+}
+
+validate_usage $@
+
+if [ "$#" -ne 3 ]; then
+    echo "[ERROR] Niepoprawna ilość argumentów wymagano 3!" >&2
+    exit 1
+fi
+
 USER_NAME=git
 WORKING_DIRECTORY=/home/${USER_NAME}/test-ci/
 FRONTEND_DIRECTORY=frontend
